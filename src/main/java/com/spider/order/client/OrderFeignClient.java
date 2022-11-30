45952481bb0d849f874f7d28f874a549681edc5e
eastflag <eastflag@gmail.com>
2022-11-30 22:04:16 +0900

    .

--- src/main/java/com/spider/order/client/OrderFeignClient.java
+++ src/main/java/com/spider/order/client/OrderFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "orderFeignClient", url = "${agent.server.url", configuration = OrderFeignClientConfig.class)
+@FeignClient(name = "orderFeignClient", url = "${agent.server.url}", configuration = OrderFeignClientConfig.class)
 public interface OrderFeignClient {
     @RequestMapping(method = RequestMethod.POST, value = "/api/neworder")
     ServerResponseDTO postNewOrder(@RequestBody ServerRequestDTO serverRequestDTO);
